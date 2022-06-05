846ba619e0d8c5a4ca1d3e420e7e7f5c29fe6c7a
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-05 16:38:54 -0700

    LastDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,8 +13,8 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is ",n);
-if (n > 5)
+printf("Last digit of %d is ", n);
+if (n >5)
 {
 	printf("greater than 5");
 }
@@ -22,7 +22,7 @@ if (n == 0)
 {
 	printf("0");
 }
-if (n < 6 && n != 0)
+if (n < 6 && n !=0)
 {
 	printf("less than 6 and not 0");
 }
