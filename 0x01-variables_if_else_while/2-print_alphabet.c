2b577ad982baeabac1798eafd760201681b09ac2
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-06 16:35:49 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <== 122; ch ++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+return (0);
+
+}
