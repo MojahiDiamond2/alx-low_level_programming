1adf9961abf3feda1219549af1ab224c7cdbe9cf
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-07 10:12:53 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	int ch,
-	    for (ch = 97; ch <= 122; ch++ : '\n')
-
-		for (ch = 65; ch <= 90; ch++ : '\n')
-
-		{
-			putchar(ch)
-		}
-		{
-			putchar(10)
-		}
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar (10); /*ascii code for new line*/
 return (0);
 }
