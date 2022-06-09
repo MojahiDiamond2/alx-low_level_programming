168b14713dad35e3b695f0f78b2c25e0bb3c7048
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-09 08:35:31 -0700

    Alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	char ch = 'a'
-	while ch <= 'z'
+	char ch = 'a';
+	while ch <= 'z';
 	{
 	if (ch != 'q' && ch != 'e')
 	{
 		putchar(ch);
 	}
-	ch++
+	ch++;
 	}
 	putchar('\n');
 	return (0);
