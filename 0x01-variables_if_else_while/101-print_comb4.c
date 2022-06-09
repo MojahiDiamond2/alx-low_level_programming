4eb4c7d00faa22cd96422e488b744b5e26421059
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-09 14:56:22 -0700

    101print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	int n, m. l;
+	int n, m, l;
 
 	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 48; l < 58; l++)
+			for (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
 				{
