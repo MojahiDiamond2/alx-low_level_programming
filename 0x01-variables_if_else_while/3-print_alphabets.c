6844b3c9c596738d614bdaeae6602706567d086e
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-09 04:59:00 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,13 +2,13 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase
- *
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int ch;
-	for (ch = 97; ch <= 122; ch++)\n
+	for (ch = 97; ch <= 122; ch++)
 	{
 		putchar(ch);
 	}
