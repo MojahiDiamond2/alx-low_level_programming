909320553f802e2bcb0b055ae48b2382dc08d7a9
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-14 13:38:58 -0700

    PrintArray

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,6 +6,7 @@
  * followed by a new line
  * @a: arrays of integers
  * @n: number of elements of the array to be printed
+ *
  * Return: void
  */
 
@@ -22,5 +23,5 @@ void print_array(int *a, int n)
 			printf(", ");
 		}
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
