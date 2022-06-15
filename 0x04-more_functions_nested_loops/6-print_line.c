2768595ff9c440956e8d644c641f854e6cce64aa
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-15 15:34:15 -0700

    PrintLine

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,15 @@
 #include "main.h"
+
+/**
+ * print_line - draws a straight line in the termina
+ * @n: integer to use
+ */
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x++)
+		_putchar('_');
+	_putchar('\n');
