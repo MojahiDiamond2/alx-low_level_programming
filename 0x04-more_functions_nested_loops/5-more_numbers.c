e27e7049047440eaf341109796ef1d54538f1149
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-15 16:17:57 -0700

    MoreNumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,16 @@
  */
 void more_numbers(void)
 {
-	int y = 15;
+	int x, y;
 
-	for (y = 0; y <= 15; y++)
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 15; y++)
 		{
-			_putchar(y + '0');
+			if (y >=10)
+				_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
+	}
 }
