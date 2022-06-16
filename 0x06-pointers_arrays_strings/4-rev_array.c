a84f4c6cae6ef9c4af8e1681dda883ddc763217d
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-16 16:58:34 -0700

    ReverseArray

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,11 @@ void reverse_array(int *a, int n)
 	b = a;
 	for (i = 0; i < n; i++)
 		b++;
-	for (k = 0; k < n / 2; k++)
+	for (k = 0; k <= n / 2; k++)
 	{
 		aux = a[k];
 		a[k] = b*;
 		*b = aux;
-		*b -= 1;
+		*b--;
 	}
 }
