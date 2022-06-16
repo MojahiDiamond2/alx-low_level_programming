cd5e4e32fefbdfa0c9133fb3156e309049ed7d9e
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-16 15:57:45 -0700

    Strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,10 +5,10 @@
  * _strcmp - compares two strings
  * @s1: first string
  * @s2: second string
- * Return: dest
+ * Return: Always 0 (Success)
  */
 int _strcmp(char *s1, char *s2)
 {
 	strcmp(s1, s2);
-	return (dest);
+	return (0);
 }
