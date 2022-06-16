d22d92bec4b1b2a89def5911e601de8170792694
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-16 15:56:12 -0700

    Strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (dest);
+}
