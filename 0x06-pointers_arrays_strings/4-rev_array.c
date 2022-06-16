e36916c49aa858e9880666263c0db1285ec75682
Kegomoditswe <kegocmojahi@gmail.com>
2022-06-16 16:32:45 -0700

    ReverseArray

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ */
+void reverse_array(int *a, int n)
+{
+	int *a, n;
+
+	reverse(*a, n);
+	print (*a, n);
+	return (0);
+}
